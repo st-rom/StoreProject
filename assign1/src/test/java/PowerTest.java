306b073aaf25bbcfab50286cea93c1d97d13aeb7
st-rom <r.stepaniuk@ucu.edu.ua>
2017-11-14 15:07:45 +0200

    Update PowerTest.java

--- assign1/src/test/java/PowerTest.java
+++ assign1/src/test/java/PowerTest.java
@@ -4,29 +4,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PowerTest {
-    private Power a;
-    private Power b;
-    private Power c;
+    private Power aa;
+    private Power bb;
+    private Power cc;
     @Before
     public void setUp() throws Exception {
-        a = Power.LOW;
-        b = Power.GREAT;
-        c = Power.GODLY;
+        aa = Power.LOW;
+        bb = Power.GREAT;
+        cc = Power.GODLY;
     }
 
     @Test
     public void testOne() throws Exception {
-        assertEquals("Low", a.toString());
+        assertEquals("Low", aa.toString());
     }
 
     @Test
     public void testTwo() throws Exception {
-        assertEquals("Great", b.toString());
+        assertEquals("Great", bb.toString());
     }
 
     @Test
     public void testThree() throws Exception {
-        assertEquals("Godly", c.toString());
+        assertEquals("Godly", cc.toString());
     }
 
 }
